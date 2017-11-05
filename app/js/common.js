a4bfb550a8995800be11dc4840fa328fb57153b6
andep91 <andep91@mail.ru>
2017-11-05 21:02:03 +0300

    05.11.2017 mmenu

--- app/js/common.js
+++ app/js/common.js
@@ -3,24 +3,24 @@ $(function() {
   if ($(document).width() <= 768){
     $('.menu').attr('id','mmenu');
     $('.menu').removeAttr('class');
+
+    $('#mmenu').mmenu({
+      'extensions': ['theme-dark'],
+      'navbar' : {
+        'title' : ''
+      },
+      'offCanvas': {
+        'position': 'right'
+      }
+    });
+
+    var api = $('#mmenu').data('mmenu');
+    api.bind('open:finish',function () {
+      $('.hamburger').addClass('is-active');
+    });
+    api.bind('close:finish',function () {
+      $('.hamburger').removeClass('is-active');    
+    });
   };
-
-  $('#mmenu').mmenu({
-    'extensions': ['theme-dark'],
-    'navbar' : {
-      'title' : ''
-    },
-    'offCanvas': {
-      'position': 'right'
-    }
-  });
-
-  var api = $('#mmenu').data('mmenu');
-  api.bind('open:finish',function () {
-    $('.hamburger').addClass('is-active');
-  });
-  api.bind('close:finish',function () {
-    $('.hamburger').removeClass('is-active');    
-  });
 
 });
